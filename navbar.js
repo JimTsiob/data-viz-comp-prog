d530900e38b6b93e73d25bc4de1cd6aeddb589b3
Tsiobieman <dtsiomp@hotmail.com>
2024-06-18 18:03:14 +0300

    small cleanup.

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 document.write('\
 <div class="topnav" id="myTopnav"> \
-  <a href="http://127.0.0.1:5500/index.html"> \
+  <a href="./index.html"> \
     <div class="index">\
       <t> ta kala </t> \
       <t style="color: #d93333"> mandata </t>\
@@ -9,31 +9,31 @@ document.write('\
   <div class="vl"></div> \
   <a id="language" href="./language.html"> \
     <div class="selected"> \
-      <img src="/images/lens.png" width="40px" height="auto" />\
+      <img class="nav_img" src="images/lens.png" width="40px" height="auto" />\
       <div>language</div>\
     </div> \
   </a> \
   <a id="problem" href="./problem.html">\
     <div class="selected"> \
-      <img src="/images/qmark_bubble.png" width="40px" height="auto" />\
+      <img class="nav_img" src="images/qmark_bubble.png" width="40px" height="auto" />\
       <div>problem</div> \
     </div> \
   </a> \
   <a id="time" href="./time.html"> \
     <div class="selected"> \
-      <img src="/images/clock.png" width="40px" height="auto" /> \
+      <img class="nav_img" src="images/clock.png" width="40px" height="auto" /> \
       <div>time</div>\
     </div> \
   </a> \
   <a id="memory" href="./memory.html"> \
     <div class="selected"> \
-      <img src="/images/memory.png" width="40px" height="auto" />\
+      <img class="nav_img" src="images/memory.png" width="40px" height="auto" />\
       <div>memory</div>\
     </div> \
   </a> \
   <a id="website" href="./website.html">\
     <div class="selected"> \
-      <img src="/images/website.png" width="40px" height="auto" /> \
+      <img class="nav_img" src="images/website.png" width="40px" height="auto" /> \
       <div>website</div> \
     </div> \
   </a> \
