a55ac22cfddb57b68887d04baa1a4cfdcaa2de4c
LoukasPap <loukas.ppd@gmail.com>
2024-06-18 16:29:37 +0300

    Add responsive navbar

---
+++ navbar.js
@@ -0,0 +1,45 @@
+document.write('\
+<div class="topnav" id="myTopnav"> \
+  <a href="http://127.0.0.1:5500/index.html"> \
+    <div class="index">\
+      <t> ta kala </t> \
+      <t style="color: #d93333"> mandata </t>\
+    </div> \
+  </a> \
+  <div class="vl"></div> \
+  <a id="language" href="./language.html"> \
+    <div class="selected"> \
+      <img src="/images/lens.png" width="40px" height="auto" />\
+      <div>language</div>\
+    </div> \
+  </a> \
+  <a id="problem" href="./problem.html">\
+    <div class="selected"> \
+      <img src="/images/qmark_bubble.png" width="40px" height="auto" />\
+      <div>problem</div> \
+    </div> \
+  </a> \
+  <a id="time" href="./time.html"> \
+    <div class="selected"> \
+      <img src="/images/clock.png" width="40px" height="auto" /> \
+      <div>time</div>\
+    </div> \
+  </a> \
+  <a id="memory" href="./memory.html"> \
+    <div class="selected"> \
+      <img src="/images/memory.png" width="40px" height="auto" />\
+      <div>memory</div>\
+    </div> \
+  </a> \
+  <a id="website" href="./website.html">\
+    <div class="selected"> \
+      <img src="/images/website.png" width="40px" height="auto" /> \
+      <div>website</div> \
+    </div> \
+  </a> \
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()"> \
+    <i class="fa fa-bars"></i> \
+  </a> \
+</div> \
+');
+
